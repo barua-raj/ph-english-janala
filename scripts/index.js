f4d25d5468eacb31b57c7b5deb456a96c191481a
barua-raj <barua.jovraj@gmail.com>
2025-09-08 13:45:39 +0600

    wip: words level js

--- scripts/index.js
+++ scripts/index.js
@@ -5,9 +5,42 @@ const loadLessons = () => {
     .then((res) => res.json())
     .then((json) => {
         displayLesson(json.data);
-    })
-}
+    });
+};
 loadLessons();
+
+const loadLevelWord = (id) => {
+    // console.log(id);
+    const url = `https://openapi.programming-hero.com/api/level/${id}`;
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayLevelWord(data.data))
+}
+
+const displayLevelWord = (words) => {
+    const wordContainer = document.getElementById("word-container");
+    wordContainer.innerHTML = "";
+
+    words.forEach(word => {
+        const card =document.createElement("div");
+        card.innerHTML = `
+            <div class="bg-white rounded-xl shadow-sm text-center py-10 px-2 space-y-4">
+                <h2 class="text-2xl font-bold">${word.word}</h2>
+                <p class="font-semibold">Meaning/Pronunciation</p>
+                <div class="font-bangla text-2xl font-medium text-[rgba(24,24,27,1)]">
+                    "${word.meaning} / ${word.pronunciation}"
+                </div>
+                <div class="flex justify-center items-center gap-85 mt-10">
+                    <button class="btn bg-[rgba(26,145,255,0.1)] hover:bg-[#1A91FF70]"><i class="fa-solid fa-circle-info"></i></button>
+                    <button class="btn bg-[rgba(26,145,255,0.1)] hover:bg-[#1A91FF70]"><i class="fa-solid fa-volume-high"></i></button>
+                </div>
+            </div>
+        `;
+        wordContainer.append(card);
+    });
+};
+
+
 
 const displayLesson = (lessons) => {
 
@@ -20,10 +53,9 @@ const displayLesson = (lessons) => {
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML = `
 
-            <button href="" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open-reader"></i> Lesson - ${lesson.level_no}</button>
+            <button onClick="loadLevelWord(${lesson.level_no})" href="" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open-reader"></i> Lesson - ${lesson.level_no}</button>
 
         `;
         levelContainer.append(btnDiv);
-    }
-
-}
+    };
+};
